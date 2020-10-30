b69b49dfdad5c254f05d49af25c179528d7fed58
simonacossai <cossaisimona@gmail.com>
2020-10-30 10:24:53 +0100

    DONE- sign up page

--- public/Sign Up/signUp.js
+++ public/Sign Up/signUp.js
@@ -1,20 +1,9 @@
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
-  });
-  // [END createwithemail]
 
+/*
   const auth = firebase.auth();
-  function signUp(){
+  function signUp(event){
+    event.preventDefault();
+console.log("blala")
       var email = document.getElementById("#email", "#email2");
       var password = document.getElementById("password");
 
@@ -23,3 +12,23 @@ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(e
 
       alert("Signed up")
   }
+*/
+
+
+let signUp = document.querySelector("#signUp");
+let email= document.querySelector("#email");
+let password= document.querySelector("#password")
+  signUp.addEventListener('click', function (event) {
+    event.preventDefault();
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+    .then(function (result) {
+      console.log("ccc")
+        window.location.replace('../index.html');
+        window.location.href = ("../index.html");
+    }).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage);
+    });
+});
+
--- public/firebase.js
+++ public/firebase.js
@@ -1,6 +1,4 @@
 
-
-
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
