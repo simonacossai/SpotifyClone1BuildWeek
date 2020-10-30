48bbd9835a7dfa611b691ec267fc6e48e5858058
simonacossai <cossaisimona@gmail.com>
2020-10-30 14:08:07 +0100

    done- adding bootstrap alert on login page

--- public/login.js
+++ public/login.js
@@ -1,38 +1,34 @@
-  let btnLogin = document.querySelector("#login");
-  let email = document.querySelector("#email");
-  let password = document.querySelector("#password");
+let btnLogin = document.querySelector("#login");
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
 
 
 btnLogin.addEventListener('click', function (event) {
-    event.preventDefault();
-    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
-    .then(function (result) {
-        loginSuccesfull();
-        window.location.replace('./homePage.html');
-        window.location.href = ("./homePage.html");
-        
+  event.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .then(function (result) {
+      loginSuccesfull();
+      window.location.replace('./homePage.html');
+      window.location.href = ("./homePage.html");
+      
 
-    }).catch(function (error) {
-        loginNotSuccesfull()
-       
-    });
+  }).catch(function (error) {
+      loginNotSuccesfull()
+     
+  });
 
 });
 
 loginSuccesfull = () => {
-    let getBalloon = document.querySelector(".addRemove");
-    getBalloon.innerText = "Succesfull Login"
-    getBalloon.classList.add("bg-success")
-    getBalloon.style = "color:white; text-align:center"
-        getBalloon.classList.remove("d-none");
+  let getBalloon = document.querySelector(".addRemove");
+  getBalloon.innerText = "Succesfull Login"
+      getBalloon.classList.remove("d-none");
 }
 loginNotSuccesfull = () =>{
-    let getBalloon = document.querySelector(".addRemove");
-    getBalloon.innerText = "Wrong Password/Name"
-    getBalloon.classList.add("bg-danger")
-    getBalloon.style = "color:white; border-color: red;text-align:center"
-        getBalloon.classList.remove("d-none");
-        setTimeout(function () {
-            getBalloon.classList.add("d-none");
-          }, 3000);
+  let getBalloon = document.querySelector(".fail");
+  getBalloon.innerText = "Wrong Password/Name"
+      getBalloon.classList.remove("d-none");
+      setTimeout(function () {
+          getBalloon.classList.add("d-none");
+        }, 3000);
 };
