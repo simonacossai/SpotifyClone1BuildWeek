c0c4888793df3d7a67aace79ab22e89011d975a9
Evgeni67 <evgeni313@abv.bg>
2020-10-30 09:17:32 -0500

    pop up messages for logins

--- public/login.js
+++ public/login.js
@@ -7,15 +7,32 @@ btnLogin.addEventListener('click', function (event) {
     event.preventDefault();
     firebase.auth().signInWithEmailAndPassword(email.value, password.value)
     .then(function (result) {
+        loginSuccesfull();
         window.location.replace('./homePage.html');
         window.location.href = ("./homePage.html");
         
 
     }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
+        loginNotSuccesfull()
+       
     });
 
 });
 
+loginSuccesfull = () => {
+    let getBalloon = document.querySelector(".addRemove");
+    getBalloon.innerText = "Succesfull Login"
+    getBalloon.classList.add("bg-success")
+    getBalloon.style = "color:white; text-align:center"
+        getBalloon.classList.remove("d-none");
+}
+loginNotSuccesfull = () =>{
+    let getBalloon = document.querySelector(".addRemove");
+    getBalloon.innerText = "Wrong Password/Name"
+    getBalloon.classList.add("bg-danger")
+    getBalloon.style = "color:white; border-color: red;text-align:center"
+        getBalloon.classList.remove("d-none");
+        setTimeout(function () {
+            getBalloon.classList.add("d-none");
+          }, 3000);
+};
